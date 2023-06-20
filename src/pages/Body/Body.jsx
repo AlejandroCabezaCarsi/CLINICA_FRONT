e1231e0f2b2c01d578c83b3da46789a82081bba1
Alex <cabezacarsialejandro@gmail.com>
2023-06-20 11:07:04 +0200

    [ADD] Imported all views in Body.jsx

---
+++ src/pages/Body/Body.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import {Routes, Route} from 'react-router-dom'
+import {Home} from '../Home/Home'
+import {Login} from '../Login/Login'
+import {Register} from '../Register/Register'
+
+export const Body = () => { 
+
+    return(
+        <>
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/Register" element={<Register/>}/>
+            <Route path="/Login" element={<Login/>}/>
+        </Routes>
+        </>
+    )
+
+}

5b7d1b51adf2fb60f0df7179b72a097fb3c5ea43
Alex <cabezacarsialejandro@gmail.com>
2023-06-20 17:50:59 +0200

    [ADD] Added the navbar. There is still work to be done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
-import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import {Body} from './pages/Body/Body'
 import { Header } from './common/Header/Header'
+import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
--- src/common/Header/Header.jsx
+++ src/common/Header/Header.jsx
@@ -1,32 +1,43 @@
 import React from "react";
 import './Header.css'; 
 import { Changeview } from "../ChangeView/ChangeView";
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import { Row } from "react-bootstrap";
 
 export const Header = () => {
 
-    return( 
-        <div className="headerDesign">
+    return (
+        <Navbar expand="lg" className="headerDesign" data-bs-theme="dark" >
+          <Container className="m-0 ">
+            
+              <Navbar.Brand href="#home" className='logo'></Navbar.Brand>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav flexDesign">
+                <Nav className="me-auto">
+                  <Nav.Link href="#home">Home</Nav.Link>
+                  <Nav.Link href="#link">Nuestro equipo</Nav.Link>
+                  <Nav.Link href="#link">Tratamientos</Nav.Link>
+                  <Nav.Link href="#link">Pide cita</Nav.Link>
 
-        <div className="logo"></div>
-
-        <div className="links">
-            <div className="tratamientos marginLinks">TRATAMIENTOS</div>
-            <div className="nuestroEquipo marginLinks">NUESTRO EQUIPO</div>
-        </div>
-
-        <div className="Botones">
-            <Changeview
-                path={"/Register"}
-                name={"Register"}
-            />
-            <Changeview
-                path={"/Login"}
-                name={"Login"}
-            />
-        </div>
-        
-        </div>
-    )
-
-
+                </Nav>
+                  <Nav className=" alignEnd">
+                  <Changeview
+                  path={"/Register"}
+                  name={"Register"}
+                />
+                <Changeview
+                    path={"/Login"}
+                    name={"Login"}
+                />
+              </Nav>
+              </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      );
 }

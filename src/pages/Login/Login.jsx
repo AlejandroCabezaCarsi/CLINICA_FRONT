76e894c5f2da2f18b41eee9aa1100e9391a3d46f
Alex <cabezacarsialejandro@gmail.com>
2023-06-23 11:42:28 +0200

    [ADD] trash

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,12 +1,44 @@
 import React from "react";
+import { useState } from "react";
 import "./Login.css";
 import { Col, Container, Row } from "react-bootstrap";
 import { InputText } from "../../common/InputText/InputText";
 
 export const Login = () => {
+  
+  
+  const [credentials, setCredentials] = useState({
+    email: "",
+    password: ""
+})
+
+ const inputHandler = (e) => {
+
+    //Ahora vamos a proceder a bindear o atar los inputs mediante
+    //la presente función handler a sus correspondientes estados en el hook, que 
+    //ahora se llama credentials.
+
+    setCredentials((prevState)=>({
+        ...prevState,
+        [e.target.name] : e.target.value
+    }));
+
+ }
+
+ const inputCheck = (e) => {
+
+    console.log(e.target.value, "soy el check....");
+    console.log(e.target.name, "soy el check....");
+
+ }
+  
   return (
     <div className="loginDesign">
-      <Container className="form mt-3 d-flex flex-column justify-content-center">
+    <div className="marginTop"></div>
+      <Container className="form mt-5">
+        
+        <pre>{JSON.stringify(credentials, null,2)}</pre>
+
         <Row>
             <Col lg={12}>
             E-mail
@@ -14,20 +46,37 @@ export const Login = () => {
         </Row>
         <Row>
             <Col lg={12}>
-            <InputText/>
+            <InputText 
+                // type, design, placeholder, name, functionHandler, onBlurFunction
+                type={"email"}
+                design={"normalInput"}
+                placeholder={"  Email..."}
+                name={"email"}
+                functionHandler={inputHandler}
+                onBlurFunction={inputCheck}
+            />
             </Col>
         </Row>
         <Row>
-            <Col lg={12}>
+            <Col lg={12} className="mt-4">
             Contraseña
             </Col>
         </Row>
         <Row>
 
             <Col lg={12}>
-            <InputText/>
+            <InputText 
+                // type, design, placeholder, name, functionHandler, onBlurFunction
+                type={"password"}
+                design={"normalInput"}
+                placeholder={"  Password..."}
+                name={"password"}
+                functionHandler={inputHandler}
+                onBlurFunction={inputCheck}
+            />
             </Col>
         </Row>
+        
       </Container>
     </div>
   );
---
+++ src/services/apiCalls.js
@@ -0,0 +1,13 @@
+export const bringProducts = async () => {
+
+    return await axios.get(`https://rickandmortyapi.com/api/character/?page=17`);
+};
+};
+
+export const loginMe = async (credentials) => {
+
+    //Esto es un ejemplo de como enviamos un body por axios en un protocolo POST
+    return await axios.post(`urldelbackendqueseencargadellogin`, credentials);
+
+
+}

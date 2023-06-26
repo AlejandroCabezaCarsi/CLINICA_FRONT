9dc29b129ea5bbb506a08f0ba0f17934e59a1f13
Alex <cabezacarsialejandro@gmail.com>
2023-06-26 13:06:14 +0200

    [ADD] navBar v1

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {Body} from './pages/Body/Body'
 import { Header } from './common/Header/Header'
 import './App.css'
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 
 function App() {
 
--- src/common/Header/Header.jsx
+++ src/common/Header/Header.jsx
@@ -1,42 +1,66 @@
 import React from "react";
 import './Header.css'; 
 import { Changeview } from "../ChangeView/ChangeView";
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import { Col, Row } from "react-bootstrap";
+import {
+  MDBContainer,
+  MDBNavbar,
+  MDBNavbarBrand,
+  MDBNavbarToggler,
+  MDBNavbarNav,
+  MDBNavbarItem,
+  MDBNavbarLink,
+  MDBCollapse,
+} from 'mdb-react-ui-kit';
+import { useNavigate } from "react-router-dom";
 export const Header = () => {
 
+  const navigate = useNavigate()
+
   return (
-    <Navbar expand="lg" className="headerDesign p-0 m-0">
-      <Container className=""> 
-        <Row>
-          <Col>
-            <Navbar.Brand className="logo"></Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" className="borde" />
-            <Navbar.Collapse id="basic-navbar-nav" data-bs-theme="light">
-              <Nav className="me-auto">
-                <Nav.Link>Nosotros</Nav.Link>
-                <Nav.Link>Reserva de citas</Nav.Link>
-                <Nav.Link>Tratamientos</Nav.Link>
-              </Nav>
-              <Nav>
-                <Changeview
-                  path={'/login'}
-                  name={'Login'}
-                />
-                <Changeview
-                  path={'/register'}
-                  name={'Register'}
-                  />
-              </Nav>
-            </Navbar.Collapse>
-          </Col>
-        </Row>
-      </Container>
-    </Navbar>
+    <MDBNavbar className='headerDesign' expand='lg'>
+      <MDBContainer fluid>
+        <MDBNavbarBrand className="text-light" onClick={()=> navigate("/")}>RisalDent</MDBNavbarBrand>
+        <MDBNavbarToggler
+          type='button'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+        
+        >
+        
+        </MDBNavbarToggler>
+        <MDBCollapse navbar>
+          <MDBNavbarNav className="alignHeaderLinks">
+            <div className="leftLinks">
+              <MDBNavbarItem>
+                <MDBNavbarLink className="text-light" active aria-current='page' href='#'>
+                  Home
+                </MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink className="text-light" >Nosotros</MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink className="text-light" >Nuestras clinicas</MDBNavbarLink>
+              </MDBNavbarItem>
+            </div>
+            <div className="rightLinks">
+            
+            <Changeview
+              name={"Login"}
+              path={"/Login"}
+            />
+            <Changeview
+            name={"Register"}
+            path={"/Register"}
+            />
+            
+            </div>
+          </MDBNavbarNav>
+        </MDBCollapse>
+      </MDBContainer>
+    </MDBNavbar>
   );
 }
 

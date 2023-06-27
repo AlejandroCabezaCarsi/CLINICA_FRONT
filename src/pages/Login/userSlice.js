630e56eebcce63838c50b5dc9714cbd35309e37d
Alex <cabezacarsialejandro@gmail.com>
2023-06-27 20:30:23 +0200

    [ADD] userSlice created

---
+++ src/pages/Login/userSlice.js
@@ -0,0 +1,53 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const userSlice = createSlice({
+    name: "user",
+    initialState: {
+        credentials:{
+            token: "",
+        },
+        dataUser:{
+            name: "",
+            role: "",
+        },
+
+    },   
+    reducers:{
+
+        login: (state,action) => {
+
+            let {payload} = action; 
+
+            state.credentials = {
+                token: payload.token
+            }
+
+            state.dataUser = {
+                name: payload.user,
+                role: payload.role
+            }
+        },
+
+        logout: (state) => {
+            return {
+                ...state,
+                credentials:{
+                    token: "",
+                },
+                dataUser:{
+                    name: "",
+                    role: "",
+                }
+            }
+        }
+
+
+
+    },
+    
+});
+
+export const userData = (state) => state.user
+export default userSlice.reducer; 
+export const {login ,logout} = userSlice.actions
+

7ed2c261cdd20f39b298adf01b4209b345ea7ad9
Alex <cabezacarsialejandro@gmail.com>
2023-06-29 00:51:37 +0200

    [ADD] Profile page created

--- src/pages/Body/Body.jsx
+++ src/pages/Body/Body.jsx
@@ -3,6 +3,7 @@ import {Routes, Route} from 'react-router-dom'
 import {Home} from '../Home/Home'
 import {Login} from '../Login/Login'
 import {Register} from '../Register/Register'
+import { Profile } from "../Profile/Profile";
 
 export const Body = () => { 
 
@@ -12,6 +13,7 @@ export const Body = () => {
             <Route path="/" element={<Home/>}/>
             <Route path="/Register" element={<Register/>}/>
             <Route path="/Login" element={<Login/>}/>
+            <Route path="/Profile" element={<Profile/>}/>
         </Routes>
         </>
     )
---
+++ src/pages/Profile/Profile.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Profile.css";
+import { Col, Container, Row } from "react-bootstrap";
+
+
+export const Profile = () => {
+
+    return(
+        <div className="profileDesign">
+
+            
+        
+        </div>
+    )
+
+}
